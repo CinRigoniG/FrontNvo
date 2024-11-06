b2c040745f60fbdce281446a372b98f4bbd55faa
MartiBZabala <martiizabala1707@gmail.com>
2024-11-06 15:38:23 -0300

    Agregando sucursales en inicio

--- src/components/DondeEstamos/DondeEstamos.tsx
+++ src/components/DondeEstamos/DondeEstamos.tsx
@@ -9,24 +9,62 @@ const DondeEstamos = () => {
                 <FontAwesomeIcon icon={faGlobe} className="icon-globe" />
                 <h2 className='title ms-2'>¿Dónde estamos?</h2>
             </div>
+            
             <p className="description">
-                Visítanos en nuestra tienda musical ubicada en el corazón de Mendoza. 
-                Ofrecemos una amplia gama de instrumentos musicales y accesorios para todos los niveles.
+                MusicShop tiene dos sucursales en Mendoza. Te invitamos a visitarnos en cualquiera de nuestras tiendas para encontrar una amplia gama de instrumentos musicales y accesorios para todos los niveles.
             </p>
-            <div className="col-md-12 d-flex justify-content-center map-container">
-                <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3350.448239960358!2d-68.84085262461177!3d-32.88631516884348!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x967e091ed2dd83f7%3A0xf41c7ab7e3522157!2sAv.%20San%20Mart%C3%ADn%20%26%20Av.%20Las%20Heras%2C%20Capital%2C%20Mendoza!5e0!3m2!1ses!2sar!4v1713369845039!5m2!1ses!2sar"
-                    width="100%"
-                    height="450"
-                    style={{ border: '0' }}
-                    allowFullScreen={true}
-                    loading="lazy"
-                    referrerPolicy="no-referrer-when-downgrade"
-                    className='map-iframe'
-                ></iframe>
+            
+            {/* Tarjetas con la misma altura */}
+            <div className="row mb-4">
+                <div className="col-md-6 d-flex align-items-stretch">
+                    <div className="card equal-card">
+                        <div className="card-body">
+                            <h4 className="card-title">Sucursal Mendoza Centro</h4>
+                            <p className="card-text">
+                                Dirección: Avenida San Martín 1000, Centro, Mendoza. Código Postal: 5500
+                            </p>
+                            <div className="map-container">
+                                <iframe
+                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3350.448239960358!2d-68.84085262461177!3d-32.88631516884348!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x967e091ed2dd83f7%3A0xf41c7ab7e3522157!2sAv.%20San%20Mart%C3%ADn%20%26%20Av.%20Las%20Heras%2C%20Capital%2C%20Mendoza!5e0!3m2!1ses!2sar!4v1713369845039!5m2!1ses!2sar"
+                                    width="100%"
+                                    height="250"
+                                    style={{ border: '0' }}
+                                    allowFullScreen={true}
+                                    loading="lazy"
+                                    referrerPolicy="no-referrer-when-downgrade"
+                                    className='map-iframe'
+                                ></iframe>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-md-6 d-flex align-items-stretch">
+                    <div className="card equal-card">
+                        <div className="card-body">
+                            <h4 className="card-title">Sucursal Los Corralitos, Guaymallén</h4>
+                            <p className="card-text">
+                                Dirección: Callejón Lemos 1300, Los Corralitos, Guaymallén. Código Postal: 5519
+                            </p>
+                            <div className="map-container">
+                                <iframe
+                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3312.979098368881!2d-68.7967462!3d-32.9979574!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x967d0f7c0752f479%3A0x9d643cd2ea1468d1!2sCallej%C3%B3n%20Lemos%2C%20Guaymall%C3%A9n%2C%20Mendoza!5e0!3m2!1ses!2sar!4v1713369985234!5m2!1ses!2sar"
+                                    width="100%"
+                                    height="250"
+                                    style={{ border: '0' }}
+                                    allowFullScreen={true}
+                                    loading="lazy"
+                                    referrerPolicy="no-referrer-when-downgrade"
+                                    className='map-iframe'
+                                ></iframe>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+
             <div className="contact-info text-center">
-                <div className="contact-bg"> {/* Contenedor para el fondo de contacto */}
+                <div className="contact-bg"> 
                     <p className="contact-text"><strong>Teléfono:</strong> (123) 456-7890</p>
                     <p className="contact-text"><strong>Email:</strong> musicShop@gmail.com</p>
                     <a href="/contacto" className="btn btn-custom">Contáctanos</a>
--- src/entities/Domicilio.ts
+++ src/entities/Domicilio.ts
@@ -1,4 +1,4 @@
-import Persona from "./Persona";
+
 
 export default class Domicilio {
     id: number = 0;
@@ -11,5 +11,4 @@ export default class Domicilio {
     localidad : string = '';
     provincia : string = '';
     pais : string = '';
-    persona : Persona = new Persona();
 }
